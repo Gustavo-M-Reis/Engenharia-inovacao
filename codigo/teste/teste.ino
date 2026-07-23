15b01ce87d6c0a9d1438f00eb05d9583caf2965e
Igor Tironi <igor.tironi@hotmail.com>
2026-07-23 09:48:04 -0300

    codigos de teste de todos os recursos

--- codigo/teste/teste.ino
+++ codigo/teste/teste.ino
@@ -1,17 +1,33 @@
 #include <MPU6050.h>
 #include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
 
 #define CHARGE_PIN 10
 #define SENSOR_PIN A0
+#define BAT_SENS A7
 #define BUZZER_PIN 6
+
+#define BOT_CIMA 2
+#define BOT_BAIXO 4
+#define BOT_CONFIRMA 3
+#define LED 5
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 
 
 uint16_t read_charge(uint8_t sensor_pin, uint8_t charge_pin);
 
 float adjust(float input, float input_min, float input_max, float output_min, float output_max);
-void demo();
 void buzzer_logic(byte state);
+
+
 
 
 //346 leitura cheia
@@ -21,7 +37,28 @@ void buzzer_logic(byte state);
 //calib 1105 - 630
 //104 - 58
 
+class Timer{
+    public:
+    unsigned long last_time;
+
+    Timer::Timer(){
+        last_time = millis();
+    }
+
+    void Timer::reset(){
+        last_time = millis();
+    }
+
+    unsigned long Timer::get(){
+        return millis() - last_time;
+    }
+
+};
+
+void move_detect();
+
 MPU6050 IMU;
+
 
 
 void setup(){
@@ -34,13 +71,72 @@ void setup(){
     pinMode(BUZZER_PIN, OUTPUT);
     digitalWrite(BUZZER_PIN, LOW);
 
+    pinMode(BOT_CIMA, INPUT_PULLUP);
+    pinMode(BOT_BAIXO, INPUT_PULLUP);
+    pinMode(BOT_CONFIRMA, INPUT_PULLUP);
+
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        while(1);
+    }
+
     if(IMU.begin()){
         Serial.println("erro ao iniciar a MPU");
         while(1);
     }
 
+    move_detect();
 
-    demo();
+    Timer Timer_LED;
+    Timer Timer_IMU;
+    int z_accel = 1;
+    while(1){
+        display.clearDisplay();
+
+        display.setTextSize(1);      // Normal 1:1 pixel scale
+        display.setTextColor(SSD1306_WHITE); // Draw white text
+        display.setCursor(0, 0);     // Start at top-left corner
+        display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+        display.print("CIMA: ");
+        display.println(!digitalRead(BOT_CIMA));
+        display.print("BAIXO: ");
+        display.println(!digitalRead(BOT_BAIXO));
+        display.print("CONFIRMA: ");
+        display.println(!digitalRead(BOT_CONFIRMA));
+        display.print("Tensao BAT: ");
+        display.println((float)analogRead(BAT_SENS) * 0.004887);
+    
+
+        display.display();
+       
+
+        if(Timer_LED.get() < 500){
+            digitalWrite(LED, HIGH);
+        }
+        else if(Timer_LED.get() >= 500 && Timer_LED.get() < 1000){
+            digitalWrite(LED, LOW);
+        }
+        else{
+            Timer_LED.reset();
+        }
+
+        
+        if(Timer_IMU.get() > 5){
+            IMU.get_sensor(ACCEL_Z, z_accel);
+            Timer_IMU.reset();
+        }
+        
+
+        if(z_accel < 0){
+            buzzer_logic(true);
+        }
+        else buzzer_logic(false);
+
+    }
     
 }
 
@@ -120,23 +216,53 @@ void buzzer_logic(byte state){
 }
 
 
-void demo(){
 
-    unsigned long timer = 0;
-    int z_accel = 0;
+void move_detect(){
+    float x_accel_filtered = 0.0, y_accel_filtered = 0.0, z_accel_filtered = 0.0;
+    int x_accel = 0, y_accel = 0, z_accel = 0;
+
+    float allarm_filter = 0.0;
+
+    Timer read_timer;
+
+    float x = 0.0, y = 0.0, z = 0.0;
+
+    float limiar = 300.0;
+
     while(1){
-        
-        if(millis() - timer > 5){
+        if(read_timer.get() > 5){
+            IMU.get_sensor(ACCEL_X, x_accel);
+            IMU.get_sensor(ACCEL_Y, y_accel);
             IMU.get_sensor(ACCEL_Z, z_accel);
-            timer = millis();
-        }
-        
+            read_timer.reset();
 
-        if(z_accel < 0){
-            buzzer_logic(true);
+            x = (float)abs(x_accel);
+            y = (float)abs(y_accel);
+            z = (float)abs(z_accel);
+
+            x_accel_filtered += (x - x_accel_filtered) * 0.3;
+            y_accel_filtered += (y - y_accel_filtered) * 0.3;
+            z_accel_filtered += (z - z_accel_filtered) * 0.3;
         }
+
+        if(x > (x_accel_filtered + limiar)){
+            allarm_filter += ((x - x_accel_filtered + limiar)) * 0.1;
+        }
+        else allarm_filter += (-allarm_filter) * 0.01;
+
+        if(y > (y_accel_filtered + limiar)){
+            allarm_filter += ((y - y_accel_filtered + limiar)) * 0.1;
+        }
+        else allarm_filter += (-allarm_filter) * 0.01;
+
+        if(z > (z_accel_filtered + limiar)){
+            allarm_filter += ((z - z_accel_filtered + limiar)) * 0.1;
+        }
+        else allarm_filter += (-allarm_filter) * 0.01;
+
+        if(allarm_filter > 1000) buzzer_logic(true);
         else buzzer_logic(false);
-    }
-  
 
+    }
 }
+
